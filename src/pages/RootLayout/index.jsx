c3a6acd4557f8aa086b824d4f09dc4acf5bed5fc
Shashwat Shaurya <shashwatshaurya.98@gmail.com>
2024-01-10 21:33:38 +0530

    styling changes

--- src/pages/RootLayout/index.jsx
+++ src/pages/RootLayout/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 import { Link, Outlet } from 'react-router-dom';
 import s from './style.module.scss'; // scss file needs to be like name.module.scss and should not be like import * from s
 
@@ -6,20 +7,27 @@ const RootLayout = () => {
     return (
         <div className={s.pageWrapper}>
             <div className={s.nav}>
-                <ul className={s.navElements}>
-                    <li className={s.navElement}>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className={s.navElement}>
-                        <Link to="/projects">Projects</Link>
-                    </li>
-                    <li className={s.navElement}>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                    <li className={s.navElement}>
-                        <Link to="/connect">Connect</Link>
-                    </li>
-                </ul>
+                <div className={s.navElements}>
+                    <div className={s.leftElements}>
+                        <div className={cx(s.nameLogo, s.navElement)}>
+                            <Link to='/'>Shashwat Shaurya</Link>
+                        </div>
+                    </div>
+                    <div className={s.rightElements}>
+                        <div className={s.navElement}>
+                            <Link to="/">Home</Link>
+                        </div>
+                        <div className={s.navElement}>
+                            <Link to="/projects">Projects</Link>
+                        </div>
+                        <div className={s.navElement}>
+                            <Link to="/blog">Blog</Link>
+                        </div>
+                        <div className={s.navElement}>
+                            <Link to="/connect">Connect</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Outlet />
             <div className={s.footer}>By Shashwat with <span className={s.heart}>&#10084;</span></div>
