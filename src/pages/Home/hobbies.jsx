216f7f0c379e31940c2422e485ccd4c89fa8b646
Shashwat Shaurya <shashwatshaurya.98@gmail.com>
2024-08-10 17:19:55 +0530

    hobby slideshow added

--- src/pages/Home/hobbies.jsx
+++ src/pages/Home/hobbies.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
+import HobbyCard from "./hobbyCard";
+import LeftIcon from "../../assets/svg/leftIcon";
+import RightIcon from "../../assets/svg/rightIcon";
+import Button from "../../components/Button";
+import { hobbies } from "./constants";
+import s from "./style.module.scss";
+
+const runSlideShow = (f, t) => setInterval(()=>f.call(), t);
+const timer = 2000;
 
 const Hobbies = (props) => {
+  const [selection, setSelection] = useState(0);
+
+  const callbackedSlideshow = useCallback(() => runSlideShow(handleRightClick, timer), [timer]);
+
+  useEffect(()=>callbackedSlideshow(), [callbackedSlideshow]);
+
+  const handleLeftClick = () => {
+    setSelection((prev) =>
+      prev - 1 >= 0 ? prev - 1 : prev - 1 + hobbies.length
+    );
+  };
+
+  const handleRightClick = () => {
+    setSelection((prev) =>
+      prev + 1 < hobbies.length ? prev + 1 : prev + 1 - hobbies.length
+    );
+  };
+
   return (
     <>
-      <h1>HOBBIES</h1>
+      <div className={s.hobbiesHeader}>Hobbies</div>
+      <div className={s.row}>
+        <Button onClick={handleLeftClick} buttonClass={s.leftArrowBtn}>
+          <LeftIcon />
+        </Button>
+        <HobbyCard
+          imgUrl={hobbies[selection]?.imgUrl}
+          imgText={hobbies[selection]?.imgText}
+        />
+        <Button onClick={handleRightClick} buttonClass={s.rightArrowBtn}>
+          <RightIcon />
+        </Button>
+      </div>
     </>
   );
 };
