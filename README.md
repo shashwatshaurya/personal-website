8c86424afb2bdd67a02f5e2233927488a45e7566
Shashwat Shaurya <shashwatshaurya.98@gmail.com>
2024-08-11 21:58:22 +0530

    added only deployment files

--- README.md
+++
@@ -1,3 +0,0 @@
-This is an n*100th attempt at making my personal website.
-Please wish me luck.
-Will try to regularly update.
--- src/common/constants.js
+++
@@ -1,4 +0,0 @@
-export const NAME_CAPS = 'SHASHWAT SHAURYA';
-export const NAME_CAMEL = 'Shashwat Shaurya';
-export const DESIGNATION = 'Software Engineer';
-export const EMAIL = 'shashwatshaurya.98+pw@gmail.com';
