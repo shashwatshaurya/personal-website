7e9f83523d3aec7ad6a639a9abd2dd442f4c97c0
Shashwat Shaurya <shashwatshaurya.98@gmail.com>
2025-07-11 17:16:24 +0530

    changes for form warning

--- src/components/TextArea/index.jsx
+++ src/components/TextArea/index.jsx
@@ -3,16 +3,23 @@ import cx from "classnames";
 import s from "./style.module.scss";
 
 const TextArea = (props) => {
-  const { propClass, placeholder, onChange, name } = props;
+  const { propClass, placeholder, onChange, name, value, required } = props;
   return (
-    <textarea className={cx(s.textArea, propClass)} placeholder={placeholder} onChange={onChange} name={name}/>
+    <textarea
+      className={cx(s.textArea, propClass)}
+      placeholder={placeholder}
+      onChange={onChange}
+      name={name}
+      value={value}
+      required={required}
+    />
   );
 };
 
 TextArea.defaultProps = {
   propClass: "",
   placeholder: "I am A textarea",
-  onChange: () => {},
+  onChange: () => { },
 };
 
 export default TextArea;
